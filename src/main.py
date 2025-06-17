b5741243cc5b92c79f5fbc9f7530e7ce5be2fa77
Anjanay <anjanayraina326@gmail.com>
2025-06-17 16:54:14 +0530

    minor code changes

--- src/main.py
+++ src/main.py
@@ -1,19 +1,18 @@
 from fastapi import FastAPI
 from routes.job_route import router as jobs_router
 from scheduler.runner import start_scheduler
+from helper.logger import Logger
 import threading
 
+log = Logger()
 app = FastAPI(title="JobPilot Backend")
 app.include_router(jobs_router)
 
 @app.on_event("startup")
 def on_startup():
-    # Start scheduler in a background thread so it doesn't block FastAPI
     thread = threading.Thread(target=start_scheduler, daemon=True)
     thread.start()
-    print("✅ Scheduler started in background.")
-
-# Optional: add shutdown event if you want to gracefully stop scheduler
+    log.info("Scheduler started in background.")
 
 if __name__ == "__main__":
     import uvicorn
